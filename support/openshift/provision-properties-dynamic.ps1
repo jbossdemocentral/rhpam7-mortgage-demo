6a9d4147e42b79bf397b9187742d24bc8ea5afaa
Duncan Doyle <Duncan.Doyle@gmail.com>
2018-07-02 18:01:08 +0200

    Minor improvements in PowerShell scripts.

--- support/openshift/provision-properties-dynamic.ps1
+++ support/openshift/provision-properties-dynamic.ps1
@@ -5,4 +5,4 @@ if (-not ([string]::IsNullOrEmpty($ARG_PROJECT_SUFFIX)))
   $PRJ_SUFFIX =  %{$OPENSHIFT_USER -creplace "[^-a-z0-9]","-"}
 }
 
-$PRJ=@("rhpam7-mortgage-$PRJ_SUFFIX","RHPAM7 Mortgage Demo","Red Hat Process Automation Manager 7 Mortgage Demo")
+$PRJ=@("rhpam7-mortgage-$PRJ_SUFFIX","RHPAM7 Mortgage Demo","$PRJ_DESCRIPTION")
