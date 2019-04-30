fff6d5ad16ece4a85817a99bd355fdb96e475977
Duncan Doyle <Duncan.Doyle@gmail.com>
2019-04-30 15:25:18 +0200

    Moved to registry.redhat.com. Demo now requires RHN login.

--- support/openshift/provision-properties-static.ps1
+++ support/openshift/provision-properties-static.ps1
@@ -2,3 +2,4 @@ $PRJ_NAME="rhpam7-mortgage"
 $PRJ_DESCRIPTION="Red Hat Process Automation Manager 7 Mortgage Demo"
 #GitHub tag referencing the image streams and templates.
 $OPENSHIFT_PAM7_TEMPLATES_TAG="7.3.0.GA"
+$IMAGE_STREAM_TAG="1.0"
