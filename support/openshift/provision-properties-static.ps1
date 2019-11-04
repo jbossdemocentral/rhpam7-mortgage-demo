4d457e42f8fc38050ccc46df3b0145d80d00b856
Duncan Doyle <Duncan.Doyle@gmail.com>
2019-11-04 11:09:21 +0100

    Update OpenShift images after change in naming standard.

--- support/openshift/provision-properties-static.ps1
+++ support/openshift/provision-properties-static.ps1
@@ -2,5 +2,5 @@ $PRJ_NAME="rhpam7-mortgage"
 $PRJ_DESCRIPTION="Red Hat Process Automation Manager 7 Mortgage Demo"
 #GitHub tag referencing the image streams and templates.
 $OPENSHIFT_PAM7_TEMPLATES_TAG="7.5.0.GA"
-$IMAGE_STREAM_TAG="1.0"
+$IMAGE_STREAM_TAG="7.5.0"
 $PAM7_VERSION="75"
--- support/openshift/provision-properties-static.sh
+++ support/openshift/provision-properties-static.sh
@@ -2,7 +2,7 @@ PRJ_NAME="rhpam7-mortgage"
 PRJ_DESCRIPTION="Red Hat Process Automation Manager 7 Mortgage Demo"
 #GitHub tag referencing the image streams and templates.
 OPENSHIFT_PAM7_TEMPLATES_TAG=7.5.0.GA
-IMAGE_STREAM_TAG=1.0
+IMAGE_STREAM_TAG=7.5.0
 PAM7_VERSION=75
 
 # For bc-clone-git-repository script.
